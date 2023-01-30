ecef7bbad381ab89219ec51a102d61bbd9db4687
MartinLindemann <martin.lindemann1964@outlook.de>
2023-01-30 16:08:40 +0100

    Panzerquartett

---
+++ src/main/kotlin/Main.Panzer.kt
@@ -0,0 +1,19 @@
+fun main(){
+    var panzer:Panzer=Panzer(70,1100.0, 10.75,75.0 )
+
+
+}
+
+
+
+
+
+
+
+
+
+
+// Quartett klassisch Zunächst werden die Karten gut gemischt und gleichmässig an die Mitspieler verteilt
+// Jeder Spieler schaut die erhaltenen Karten an.Sollte er jetzt schon ein Quartett besitzen( d.h von einem Buchstaben
+// alle Karten 1-4) darf er das Quartett vor sich hin auslegen. Gespielt wird im Uhrzeigersinn. Wer links vom Geber
+// sitzt beginnt.
---
+++ src/main/kotlin/Panzer.kt
@@ -0,0 +1,7 @@
+class Panzer (
+    var geschwindigkeit:Int,
+    var leistung:Double,
+    var laenge:Double,
+    var gewicht:Double
+){
+}
