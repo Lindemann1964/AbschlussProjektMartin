efd06ef14df1863cf1f0f364bb2f963dfa16dcdb
MartinLindemann <martin.lindemann1964@outlook.de>
2023-01-31 15:46:15 +0100

    Update Liste Panzer

--- src/main/kotlin/Panzer.kt
+++ src/main/kotlin/Panzer.kt
@@ -1,7 +1,20 @@
-class Panzer (
-    var geschwindigkeit:Int,
-    var leistung:Double,
-    var laenge:Double,
-    var gewicht:Double
-){
+class Panzer {
+    var geschwindigkeit: Int
+    var leistung: Double
+    var laenge: Double
+    var gewicht: Double
+    var reichweite: Int
+    var name: String
+    var eckZeichen: String
+                                                      //  fünf Eigenschaften von A1 bis > H 4
+constructor(geschwindigkeit: Int, leistung: Double, laenge: Double, gewicht: Double, reichweite: Int,name:String,eckZeichen:String) {
+    this.geschwindigkeit = geschwindigkeit
+    this.leistung = leistung
+    this.laenge = laenge
+    this.gewicht = gewicht
+    this.reichweite = reichweite
+    this.name = name
+    this.eckZeichen=eckZeichen
+
+}
 }
